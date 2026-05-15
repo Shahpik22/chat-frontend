f5a7142f078d012947a5e97dc7a3dec24e041740
Shahpik22 <cdcshah@gmail.com>
2026-05-15 15:27:49 +0800

    Update code this

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -10,17 +10,25 @@ export class ChatService {
 
   // CHANGE THIS TO YOUR BACKEND URL
   // private API = 'http://localhost:3000';
- private API = 'https://chat-backend-phi-three.vercel.app';
-  
-  constructor(private http: HttpClient) {}
+  private API = 'https://chat-backend-phi-three.vercel.app';
+
+  constructor(private http: HttpClient) { }
+
+  //   getMessages(after?: string) {
+  //   let url = `${this.API}/messages`;
+  //   if (after) {
+  //     url += `?after=${after}`;
+  //   }
+  //   return this.http.get<any[]>(url);
+  // }
 
   getMessages(after?: string) {
-  let url = `${this.API}/messages`;
-  if (after) {
-    url += `?after=${after}`;
+    let url = `${this.API}/messages`;
+    if (after && after !== '') {
+      url += `?after=${after}`;
+    }
+    return this.http.get<any[]>(url);
   }
-  return this.http.get<any[]>(url);
-}
 
   sendMessage(data: any) {
     return this.http.post(`${this.API}/messages`, data);
